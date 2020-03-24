aa5be5fa537c45fbf4469e9e456ff6ee57a9336b
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-03-24 09:15:16 +0200

    2.15: puhelinluettelo step7

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -44,11 +44,17 @@ const App = () => {
     if (exists) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat(personObject))
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          console.log(response)
+          setPersons(persons.concat(response.data))
+        })
     }
     setNewName('')
     setNewNumber('')
   }
+
 
   return (
     <div>
